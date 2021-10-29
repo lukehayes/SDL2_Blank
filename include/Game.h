fb6c212210851aca9c3757a4350954043b31caa8
lukehayes <lukepurplehayes@gmail.com>
2021-10-29 16:58:55 +0100

    Added processInput() function to Game.h

--- include/Game.h
+++ include/Game.h
@@ -3,13 +3,27 @@
 
 void update(float dt)
 {
-    printf("oinoin");
+    printf("Delta: %f \n", dt);
 }
 
 
 void render()
 {
+    printf("Rendering \n");
 }
 
+void processInput()
+{
+    /* Poll for and process events */
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_q)
+        {
+            SDL_Quit();
+            // End the loop.
+        }
+    } // End Event Checks
+}
 
 #endif
